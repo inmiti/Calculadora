e2470d064ee9dd7845ebe502808aaab7786d4b52
inmiti <inmabauvel@gmail.com>
2024-05-03 12:32:50 +0200

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by ibautista on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
